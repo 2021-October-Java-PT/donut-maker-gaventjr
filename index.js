8054fd9c23283e08d6617147163243fa7dbb3465
George Avent <gaventjr@gmail.com>
2022-01-19 13:16:54 -0500

    clicking

--- index.js
+++ index.js
@@ -1,2 +1,49 @@
-const container = (document.querySelector('.container').innerText =
-  'mmmmmmmmm doughnuts.... ahhhhhhh!');
+let score = 0;
+
+score = score + 1;
+
+let donutMultiplier = 10;
+let multipliers = 0;
+let autoClicker = 100;
+let clickers = 0;
+let clickingPower = 1;
+
+//Functions
+
+function addToScore(amount) {
+  score = score + amount;
+  document.getElementById("score").innerHTML = score;
+}
+
+function buyMultiplier() {
+  if (score >= donutMultiplier) {
+    score = score - donutMultiplier;
+    multipliers = multipliers + 1;
+
+    document.getElementById("score").innerHTML = score;
+    document.getElementById("donutMultiplier").innerHTML = donutMultiplier;
+    document.getElementById("multipliers").innerHTML = multipliers;
+  }
+}
+
+function buyAutoClicker() {
+  if (score >= autoClicker) {
+    score = score - autoClicker;
+    clickers = clickers + 1;
+
+    document.getElementById("score").innerHTML = score;
+    document.getElementById("autoClicker").innerHTML = autoClicker;
+    document.getElementById("clickers").innerHTML = clickers;
+  }
+}
+
+setInterval(function () {
+  score = score + multipliers;
+  document.getElementById("multipliers").innerHTML = score;
+}, 1000);
+
+setInterval(function () {
+  score = score + multipliers;
+  score = score + clickers + 1;
+  document.getElementById("clickers").innerHTML = score;
+}, 1000);
