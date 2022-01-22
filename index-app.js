e0e55b43a5de5589007a5711b5379e8ea85fc21d
George Avent <gaventjr@gmail.com>
2022-01-21 23:37:22 -0500

    I swear this worked, help me Obi-wan!

--- index-app.js
+++ index-app.js
@@ -62,15 +62,28 @@ function enableAutoClicker() {
   }
 }
 
-//saving and resetting
+// Get the modal
+var modal = document.getElementById("myModal");
 
-function saveGame()
-let gameSave ={
-  score: score,
-  donutMultiplier: donutMultiplier, 
-  multipliers: multipliers,
-  autoClicker: autoClicker,
-  clickers : clickers,
-  clickingPower: clickingPower
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function () {
+  modal.style.display = "block";
 };
-localStorage.setItem("gameSave", JSON.stringify(gameSave)); //Turns game data into a string to be able to save
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
